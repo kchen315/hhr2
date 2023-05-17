df359bbf17d661129cc3728a3c8b2f6fadea7b46
Kevin A Chen <kchen315@longleaf-login5.its.unc.edu>
2023-05-17 11:54:53 -0400

    bulk commit

--- source/search.sh
+++ source/search.sh
@@ -1,14 +1,17 @@
-# sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionResNetV2 --weights imagenet"
+#sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionResNetV2 --weights imagenet"
 # sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionResNetV2 --weights radimagenet"
 
-sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name ResNet50 --weights imagenet"
+ #sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name ResNet50 --weights imagenet"
 # sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name ResNet50 --weights radimagenet"
 
-# sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionV3 --weights imagenet"
+#sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionV3 --weights imagenet"
 # sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name InceptionV3 --weights radimagenet"
 
-# sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name DenseNet121 --weights imagenet"
+#sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name DenseNet121 --weights imagenet"
 # sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name DenseNet121 --weights radimagenet"
 
-# sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name Xception"
-# sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name BiT"
+#sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name Xception"
+#sbatch -N 1 -n 1 -c 1 -p volta-gpu --mem=16g -t 168:00:00 --qos gpu_access --gres=gpu:1 --mail-user=kevin.chen@unchealth.unc.edu --wrap="python3 rin.py --model_name BiT"
+# echo "hello"
+
+sbatch -p general -N 1 --mem=16g -n 1 -t 168:00:00 --wrap="python3 ../data/frame_gen.py"
